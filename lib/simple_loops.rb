067fe781cd7087b32d906bff174ad387c9114a12
Sean LaFlam <sean.laflam@gmail.com>
2020-08-17 16:14:13 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,5 +14,22 @@ def loop_message_n_times(message, n)
   end
 end
 
+def output_array(array)
+  i = 0 
+  while i < array.length 
+    puts array[i]
+    i += 1
+  end
+end
 
-  
+def return_string_array(array)
+  new_arr = []
+  i = 0 
+  while i < array.length
+    new_arr << array[i].to_s 
+    i += 1 
+  end
+  return new_arr
+end
+    
+    
